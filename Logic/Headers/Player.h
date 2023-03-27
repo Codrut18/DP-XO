1e3c78fd33051c3f401d20b7e845781ca7b60c91
Codrut18 <codrut.plaiasu35@gmail.com>
2023-03-27 16:51:38 +0300

    Created doxygen documentation for 'Player' class.

--- Logic/Headers/Player.h
+++ Logic/Headers/Player.h
@@ -9,16 +9,35 @@
 #include <vector>
 #include "Board.h"
 
+/**
+ * @brief This class contains information about the player
+ * @mark represents with what sign the player plays
+ */
 class Player {
     char mark = 'u';
 
 public:
+    /**
+     * @brief This is a constructor that takes as a parameter the sign the player has
+     * @param s represents the player will play with
+     * initializes @mark with the value of the parameter
+     */
     Player(char s);
 
+    /**
+     *@brief This function places a sign on the game board at the specified position
+     * @param line represents the line on which the player is going to place a sign
+     * @param col represents the column on which the player is going to place a sign
+     * @param board represents the actual board in which the game is taking place
+     */
     void makeMove(int line, int col, std::vector<std::vector<int>> &board);
 
     void makeMove(int line, int col, Board &board);
 
+    /**
+     * This is a getter for the mark of the player
+     * @return the mark of the player
+     */
     char GetMark() const;
 };
 
